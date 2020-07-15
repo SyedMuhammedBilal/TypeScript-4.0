2fc10473a27bd18be5e8940a7e9e4a74e8d11b80
Syed Muhammed Bilal <intergo878@gmail.com>
2020-07-15 23:36:19 +0500

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,16 @@
+"use strict";
+console.log('Hello from TS');
+//let a = 1
+//a = true
+//a = [1, 2, 3]
+//let std = {}
+//console.log(std.name)
+//function a(b) {
+//  return b / 2
+//};
+//a("2"); 
+var firstName = "Syed";
+var lastName = "Bilal";
+var age = 18;
+var isActive = true;
+var courses = ["TS", "JS", "REACT"];
---
+++ src/app.ts
@@ -0,0 +1,20 @@
+console.log('Hello from TS');
+
+//let a = 1
+//a = true
+//a = [1, 2, 3]
+
+//let std = {}
+//console.log(std.name)
+
+//function a(b) {
+//  return b / 2
+//};
+
+//a("2"); 
+
+let firstName: string = "Syed";
+let lastName: string = "Bilal";
+let age: number = 18;
+let isActive: boolean = true;
+let courses: string[] = ["TS", "JS", "REACT"];
